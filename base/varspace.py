6f97d128deb34a73f1c09256456d129cf4dedcd2
Damir Kadyrov <damirikadyrov@gmail.com>
2018-06-09 17:38:23 +0300

    extend script with class implemenation

--- base/varspace.py
+++ base/varspace.py
@@ -1,6 +1,47 @@
 
 import sys
 
+def varspaceWithClass():
+    n=int(sys.stdin.readline())
+    class Space:
+        def __init__(self, parent, name):
+            self.parent = parent
+            self.name = name
+            self.vars = []
+        
+        def addVar(self, var):
+            self.vars.append(var)
+
+        def findVar(self, var):
+            if var in self.vars:
+                return self.name
+            elif self.parent == None:
+                return "None"
+            else:
+                return self.parent.findVar(var)
+    space = {'global': Space(None, 'global')}
+    for i in range(n):
+        cmd, namespace, var = sys.stdin.readline().strip().split(" ")
+        if cmd == 'add':
+            if namespace in space:
+                space[namespace].addVar(var)
+            else:
+                # Log error message
+                # print("Parent space not exists: {}. Space: {}".format(namespace, space))
+                continue
+        elif cmd == 'create':
+            if namespace in space:
+                # Log error message
+                # print("Name space exists: {}. Space: {}".format(namespace, space))
+                continue
+            else:
+                space[namespace] = Space(space[var], namespace)
+        elif cmd == 'get':
+            if namespace in space:
+                print("{}".format(space[namespace].findVar(var)))
+            else:
+                print('None')
+
 def varspace():
     n = int(sys.stdin.readline())
     # debug info
@@ -36,4 +77,6 @@ def varspace():
                     namespace = space[namespace]["parent"]
 
 if __name__ == "__main__":
-    varspace()
+    # remove the commentary for the desired implementation
+    # varspaceWithClass()    
+    # varspace()
